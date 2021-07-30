fbb486c8397b6c19e7956c88798b5618e013eb5d
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-07-30 16:29:11 +1000

    improve the training process

--- api/Personalizer/__init__.py
+++ api/Personalizer/__init__.py
@@ -1,7 +1,7 @@
 import json
 import os
 from azure.cognitiveservices.personalizer import PersonalizerClient
-from azure.cognitiveservices.personalizer.models import RankableAction, RankRequest, RewardRequest
+from azure.cognitiveservices.personalizer.models import RankableAction, RankRequest
 from msrest.authentication import CognitiveServicesCredentials
 
 import os
@@ -12,16 +12,17 @@ import pandas as pd
 import azure.functions as func
 from ..utils import debuggable
 
+
 SHORTHANDS = {"E.": "Eucalyptus", 
          "L.":"Lophostemon",
          "C.": "Corymbia",
          "A.": "Angophora"}
 TRAINING_MODE = False
 
-def get_options(df: pd.DataFrame):
+def get_options(df: pd.DataFrame, area):
     options = []
 
-    for n in df['TreeSpecies'].dropna().unique():
+    for n in df[df['SurveyArea'] == area]['TreeSpecies'].dropna().unique():
         family = n
         for k, v in SHORTHANDS:
             if n.startswith(k):
@@ -31,11 +32,23 @@ def get_options(df: pd.DataFrame):
     return options
 
 
-def train(client, df, options):
+def train(client, df):
+    
+
     # Train the model , shouldn't do this every time
     for _, row in df.iterrows():
         eventid = str(uuid.uuid4())
         context = [{'area': row['SurveyArea']}, {'habitat': row['BushlandOrUrban']}]
+
+        options = []
+
+        for n in df[df['SurveyArea'] == row['SurveyArea']]['TreeSpecies'].dropna().unique():
+            family = n
+            for k, v in SHORTHANDS:
+                if n.startswith(k):
+                    family = v
+            options.append(RankableAction(id=n, features=[{'tree_family': str(family), 'genus': 'Myrtaceae'},]))
+
         rank_request = RankRequest(actions=options, context_features=context, event_id=eventid)
         response = client.rank(rank_request=rank_request)
         reward = 0
@@ -61,10 +74,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         region = req.params['region']
         habitat = req.params['habitat']
 
-        options = get_options(df)[:50]
-
-        if TRAINING_MODE:
-            train(client, df, options)
+        options = get_options(df, region)[:50]
 
         # Get a recommendation for the current input
         eventid = str(uuid.uuid4())
@@ -82,6 +92,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         try:
             response = float(req.params['response'])
             client.events.reward(event_id=id, value=response)
+            return func.HttpResponse(
+                "OK",
+                status_code=200
+            )
         except ValueError:
             return func.HttpResponse(
                 "bad response value",
@@ -92,3 +106,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 "invalid action",
                 status_code=400
             )
+
+
+if __name__ == "__main__":
+    client = PersonalizerClient(os.environ['PERSONALIZER_ENDPOINT'], CognitiveServicesCredentials(os.environ['PERSONALIZER_KEY']))
+    df = pd.read_csv('data/koala-survey-sightings-data.csv', encoding='utf-8', parse_dates=[['Date', 'Time']])
+    train(client, df)
