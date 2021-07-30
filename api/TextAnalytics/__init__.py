f5639fb26eb20d549be3a133fd574471302bddaf
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-07-30 11:45:34 +1000

    add a text analytics demo

---
+++ api/TextAnalytics/__init__.py
@@ -0,0 +1,45 @@
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+import azure.functions as func
+import json
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse: 
+    species = req.params['species']
+    with open('data/plants.json') as plants_index:
+        plants = json.load(plants_index)
+
+    if species not in plants:
+        return func.HttpResponse(
+            "Plant not recognized",
+            status_code=400
+        ) 
+
+    plant_info = os.path.join('data', 'apni', plants[species]['id'] + '.txt')
+    if not os.path.exists(plant_info):
+        return func.HttpResponse(
+            "No information on this plant",
+            status_code=400
+        )
+
+    with open(plant_info) as p:
+        plant_description = p.read()
+
+    endpoint = os.environ["TEXT_ANALYTICS_ENDPOINT"]
+    key = os.environ["TEXT_ANALYTICS_KEY"]
+
+    text_analytics_client = TextAnalyticsClient(endpoint=endpoint, credential=AzureKeyCredential(key))
+
+    articles = [plant_description]
+    results = []
+    result = text_analytics_client.extract_key_phrases(articles)
+    for _, doc in enumerate(result):
+        if not doc.is_error:
+            results.extend(doc.key_phrases)
+
+    
+    return func.HttpResponse(
+            json.dumps({"text": plant_description, "key_phrases": results}),
+            status_code=200
+        ) 
